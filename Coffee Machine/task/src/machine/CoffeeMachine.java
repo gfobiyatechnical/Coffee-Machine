2b1cf4a77aaafcb5f9d0017a310d535188b3aae5
gfobiyatechnical <Yaduvanshi#008>
2020-05-11 22:31:08 +0530

    some updates

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,51 +3,47 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+    public void espresso(){
+        int water = 250;
+        int coffeeBeans = 16;
+        int cost = 4;
+
+    }
+    public void latte(){
+        int milk = 75;
+        int water = 350;
+        int cost = 7;
+        int coffeeBeans = 12;
+
+    }
+    public void cappuccino(){
+        int milk = 100;
+        int water = 200;
+        int coffeeBeans = 12;
+        int cost = 6;
+
+    }
+    public void latte(){
+        int coffee = 250;
+        int water = 16;
+        int cost = 4;
+
+    }
+}
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         //amount of each ingredient per one cup
-        int WATER = 200;
-        int MILK = 50;
-        int BEANS = 15;
-
-        System.out.print("Write how many ml of water the coffee machine has: ");
-        int waterLevel = scanner.nextInt();
-        System.out.print("Write how many ml of milk the coffee machine has: ");
-        int milkLevel = scanner.nextInt();
-        System.out.print("Write how many grams of coffee beans the coffee machine has: ");
-        int beanLevel = scanner.nextInt();
-        System.out.print("Write how many cups of coffee you will need: ");
-        int cupsNeed = scanner.nextInt();
-        int cupsWater = 0;
-        int cupsMilk = 0;
-        int cupsBeans = 0;
-
-        if(waterLevel > WATER){
-            cupsWater = waterLevel / WATER;
-            if(milkLevel > MILK){
-                cupsMilk = milkLevel / MILK;
-            }
-            if(beanLevel > BEANS){
-                cupsBeans = beanLevel / BEANS;
-            }
-        }
-
-        int cupsMax = Math.min(Math.min(cupsWater, cupsMilk), cupsBeans);
-
-        if (cupsNeed > cupsMax){
-            System.out.println("No, I can make only " + cupsMax + " cup(s) of coffee");
-        }
-        else if (cupsNeed < cupsMax){
-            System.out.println("Yes, I can make that amount of coffee (and even "
-                    + (cupsMax - cupsNeed) + " more than that");
-        }
-        else {
-            System.out.println("Yes, I can make that amount of coffee");
-        }
-
-
-
-
+        System.out.println("The coffee machine has:\n400 of water\n540 of milk\n120 of coffee beans\n9 of disp\n");
+        System.out.println("Write action (buy, fill, take):");
+        String input = scanner.next();
+        switch (input){
+            case "take": System.out.println("I gave you $550\n\nThe coffee machine has:\n400 of water\n540 of milk\n120 of coffee beans\n9 of disposable cups\n0 of money");
+                break;
+            case "fill": System.out.println("");
+                break;
+            case "buy": System.out.println("");
+                break;
+            default: System.out.println("");
     }
 }
